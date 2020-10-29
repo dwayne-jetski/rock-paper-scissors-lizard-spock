1093340d0923a239fe3672b83e114905bea3d06f
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2020-10-28 19:44:22 -0500

    created Game, Player classes, and created Human and Computer subclasses of Player

--- RPSLS.js
+++ RPSLS.js
@@ -1,1 +1,39 @@
 "use strict"
+
+class Game {
+    constructor(){
+        this.player1 = null;
+        this.player2 = null;
+        this.gestureArray = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+        this.gestureArray = [];
+    }
+    
+}
+
+
+
+class Player {
+
+    constructor(){
+        this.name = "";
+        this.wins = 0;
+        this.gesture = null;
+    }
+}
+
+class Human extends Player{
+
+    constructor(name){
+        super();
+        this.name = name
+    }
+}
+
+class Computer extends Player{
+
+    constructor(){
+        super();
+        this.name = "HAL 3000";
+    }
+
+}
